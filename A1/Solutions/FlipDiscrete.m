a7c27f38eb3fcabefb9aa783b1d1703fb0820b3d
RocketSumit <sumitpatidar96@gmail.com>
2017-09-11 22:25:04 +0530

    FlipDiscrete function completed

--- A1/Solutions/FlipDiscrete.m
+++ A1/Solutions/FlipDiscrete.m
@@ -1,10 +1,10 @@
 function A = FlipDiscrete(B)
     
     [m n] = size(B);
-    if(m != 2 || n != 2){
-        disp('The matrix dimensions are not correct');
-        exit;
-    }
+    if(m != 2 || n != 2)
+        disp('Error: The matrix dimensions are not correct');
+        A = 'NULL';
+    
     else
     A = [B(:,1) (B(:,2)*-1)];
     end
